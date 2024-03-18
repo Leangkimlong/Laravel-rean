002f3ca3f19790b1679f41680f41c994c4db03e3
Leangkimlong <mizterlong95@gmail.com>
2024-03-18 14:09:51 +0700

    seventh commit

--- routes/web.php
+++ routes/web.php
@@ -4,38 +4,64 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 
-Route::get('/',function(){
-    // How to fetch data 
-    // how to fetch data by using emil
-    // $user = DB::select("select * from users where email = ?",["mizterlong95@gmail.com"]);// how to fetch data by using emil
-    // how to fetch data from users table using id
-    // $user = DB::select("select * from users where id = 1");
-    // fetch all users
-    $user = DB::select("select * from users");
+// CRUD with mysql statement
+// Route::get('/',function(){
+//     // How to fetch data 
+//     // how to fetch data by using emil
+//     // $user = DB::select("select * from users where email = ?",["mizterlong95@gmail.com"]);// how to fetch data by using emil
+//     // how to fetch data from users table using id
+//     // $user = DB::select("select * from users where id = 1");
+//     // fetch all users
+//     $user = DB::select("select * from users");
 
-    // How to insert data into table
-    // $user = DB::insert("insert into users (name, email, password) value (?,?,?)", ['Lay', 'mizterlay00@gmail.com', '12345678']);
+//     // How to insert data into table
+//     // $user = DB::insert("insert into users (name, email, password) value (?,?,?)", ['Lay', 'mizterlay00@gmail.com', '12345678']);
 
-    // How to update data
-    // How to update the existing data using name
-    // $user = DB::update(
-    //     "update users set email = 'mizterlong00@gmail.com' where name = ?",
-    //     ['Long']
-    // );
-    // $user = DB::update(
-    //     "update users set email = ? where name = ?",
-    //     ['mizterlong01@gmail.com', 'Long00']
-    // );
-    // How to update existing data using id
-    // $user = DB::update(
-    //     "update users set name = 'Long00' where id = 1"
-    // );
+//     // How to update data
+//     // How to update the existing data using name
+//     // $user = DB::update(
+//     //     "update users set email = 'mizterlong00@gmail.com' where name = ?",
+//     //     ['Long']
+//     // );
+//     // $user = DB::update(
+//     //     "update users set email = ? where name = ?",
+//     //     ['mizterlong01@gmail.com', 'Long00']
+//     // );
+//     // How to update existing data using id
+//     // $user = DB::update(
+//     //     "update users set name = 'Long00' where id = 1"
+//     // );
 
-    // How to delete data from table
-    // $user = DB::delete("delete from users where id = ?",[1]);
-    // $user = DB::delete("delete from users where id = 2");
+//     // How to delete data from table
+//     // $user = DB::delete("delete from users where id = ?",[1]);
+//     // $user = DB::delete("delete from users where id = 2");
 
-    // display the data in user table in array formation
+//     // display the data in user table in array formation
+//     dd($user);
+// });
+
+// CRUD with build in query (collection of array)
+Route::get('/',function() {
+    // Get function or read
+    // $user = DB::table('users')->get();
+    // $user = DB::table('users')->where('id', 3)->get(); // with where condition
+
+    // Create function or insert 
+    // $user = DB::table('users')->insert([
+    //     'email' => 'mizterlong02@gmail.com',
+    //     'name' => 'long02',
+    //     'password' => '12345678'
+    // ]);
+
+    // Update function 
+    // $user = DB::table('users')->where('id',3)->update([
+    //     'name' => 'Lay02',
+    //     'email' => 'mizterlay02@gmail.com'
+    // ]);
+
+    // Delete function or remove
+    // $user = DB::table('users')->where('id',3)->delete();
+
     dd($user);
 });
 
