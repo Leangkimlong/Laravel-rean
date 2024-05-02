dd422d4d9d5b91e0cc817536abe48557ec255100
Leangkimlong <mizterlong95@gmail.com>
2024-05-02 10:33:10 +0700

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,7 +93,7 @@
                                             <h2 class="text-xl font-semibold text-black dark:text-white">Documentation</h2>
 
                                             <p class="mt-4 text-sm/relaxed">
-                                                Welcome to K.L Store
+                                                Welcome to laravel courses
                                             </p>
                                         </div>
                                     </div>
