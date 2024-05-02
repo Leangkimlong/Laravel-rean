dd7ec3831d90b4015ea75ca9de1dcfa0646fad8b
Leangkimlong <mizterlong95@gmail.com>
2024-05-02 13:16:15 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,41 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
+use function Laravel\Prompts\select;
+
 Route::get('/', function () {
-    return view('welcome');
+    // Use to show the welcome.blade.php file content
+    // return view('welcome');
+
+    // DB facades running sql quries
+
+    // Select all user
+    $users = DB::select("select * from users");
+
+    // Select user by id
+    // $users = FB::select("select * from users where id = 1");
+    // $users = DB::select("select *from users where id = ?", [1]);
+
+    // Select user by email
+    // $users = DB::select("select * from users where email = 'kimlong@gmail.com'");
+    // $users = DB::select("select * from users where email = ?", ['kimlong@gmail.com']);
+
+    // Create new user 
+    // $users = DB::insert("insert into users (id, name, email, password) values (?,?,?,?)", [1, 'Kimlong1','kimlong11@gmail.com', "123456789"]);
+
+    // Update the user
+    // $users = DB::update("update users set name = 'kimlong95' where email = ?", ['kimlong11@gmail.com']);
+    // $users = DB::update("update users set email = ? where id = ?",
+    // ['dara@gmail.com', 6]);
+
+    // Delete user 
+    // $users = DB::delete("delete from users where id = ?", [6]);
+
+    // Show the sql quries
+    dd($users);
 });
 
 Route::get('/dashboard', function () {
